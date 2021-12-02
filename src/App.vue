4307376690026d50c7e264e9eb5a058ec33666fc
callezenwaka <callisezenwaka@gmail.com>
2021-12-02 11:14:19 +0100

    Use option api

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="app">
+    <p>{{ name }} - {{ code }}</p>
+    <button @click="changeName('Eko')">Change Name</button>
+    <button @click="changeCode('2')">Change Code</button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'App',
+  components: {
+  },
+  data() {
+    return {
+      name: 'Lagos',
+      code: 1 as number | string,
+    }
+  },
+  methods: {
+    changeName(name: string) {
+      this.name = name;
+    },
+    changeCode(code: number | string) {
+      this.code = code;
+    }
+  }
+});
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
